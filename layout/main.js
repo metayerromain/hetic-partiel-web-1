08547593de92f0a257fc0d0426998f107abb33de
metayerromain <romain.metayer@hetic.net>
2018-06-14 12:28:14 +0200

    menu mobile Features #3

--- layout/main.js
+++ layout/main.js
@@ -2,6 +2,7 @@ var scroll = window.scrollY;
 var header = document.querySelector(".row0");
 var header1 = document.querySelector(".row1");
 var backtotop = document.querySelector("#backtotop");
+var mainav = document.querySelector("#mainav");
 
 var toggle = function() {
   header1.classList.toggle('hideBar', scroll < scrollY);
@@ -17,4 +18,17 @@ window.addEventListener("scroll", function(){
   } else {
     backtotop.style.visibility = "hidden";
   }
+});
+
+var burger = document.querySelector(".burger");
+var b1 = document.querySelector(".b1");
+var b2 = document.querySelector(".b2");
+var b3 = document.querySelector(".b3");
+
+burger.addEventListener("click", function(){
+  b1.classList.toggle("b1-anim");
+  b2.classList.toggle("b2-anim");
+  b3.classList.toggle("b3-anim");
+  document.querySelector(".ul").classList.toggle("navLiMobile");
+  mainav.classList.toggle("navLiMobile");
 });

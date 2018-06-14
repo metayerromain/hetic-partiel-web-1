a3d6054f0484665d866e2c7209e58c90ca6b1d55
metayerromain <romain.metayer@hetic.net>
2018-06-14 11:56:22 +0200

    feature #1

--- layout/main.js
+++ layout/main.js
@@ -1,6 +1,7 @@
 var scroll = window.scrollY;
 var header = document.querySelector(".row0");
 var header1 = document.querySelector(".row1");
+var backtotop = document.querySelector("#backtotop");
 
 var toggle = function() {
   header1.classList.toggle('hideBar', scroll < scrollY);
@@ -9,3 +10,11 @@ var toggle = function() {
 }
 
 window.onscroll = toggle;
+
+window.addEventListener("scroll", function(){
+  if (scroll > 3000) {
+    backtotop.style.visibility = "visible";
+  } else {
+    backtotop.style.visibility = "hidden";
+  }
+});
